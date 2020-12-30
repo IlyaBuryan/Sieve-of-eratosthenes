5013656ba25b63b5a07cb3b94a3cf8c47f9313af
Ilya <forwow21@mail.ru>
2020-12-30 15:37:29 +0300

    Creation

---
+++ Sieve-of-eratosthenes.py
@@ -0,0 +1,15 @@
+"""
+Позволяет эффективно найти необходимое по счету (№N) простое число.
+Allows to effectively find an №N prime number
+"""
+
+
+def erat_fun(n):
+    """With Sieve-of-eratosthenes"""
+    prime = [2, 3]
+    i = 4
+    while len(prime) < n:
+        if all(map(lambda k: i % k != 0, prime)):
+            prime.append(i)
+        i += 1
+    return prime[-1]
